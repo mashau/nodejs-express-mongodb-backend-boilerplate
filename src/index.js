c9dd82b87abd7d76ca78ac201a26b5efdb382c49
mashau <samwelsh809@gmail.com>
2022-07-14 14:02:50 +0300

    import express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("server is working");
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+    console.log("server listening on port:", process.env.PORT);
+})
+
